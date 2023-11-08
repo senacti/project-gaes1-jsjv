944e66a1977f2d9591738471521a36a026cd4c7a
valentinasc30 <105883219+valentinasc30@users.noreply.github.com>
2023-11-08 13:51:44 -0500

    MOdale

---
+++ 06-Trimestre 6/JSJV/inventario/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models import Machine,Inventory,Input
+
+@admin.register(Input)
+class InputAdmin(admin.ModelAdmin):
+    list_display = ('product_type',)
+    
+
+
+@admin.register(Machine)
+class MachineAdmin(admin.ModelAdmin):
+   list_display = ('machine_type',)
+  
+
+
+
+@admin.register(Inventory)
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ('supplier_name', 'amount','input','machine',)
+    list_display_links=('supplier_name',)
+    list_editable=('amount',)
+    search_fields=('supplier_name',)
+    list_filter=('input','machine',)
+    list_per_page=2
+#admin.site.register(Machine)
+#admin.site.register(Inventory)
+#admin.site.register(Input)
+
