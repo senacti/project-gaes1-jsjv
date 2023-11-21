519e8393a6be9aa4a8470c3a33900d86eba5fbc5
valentinasc30 <105883219+valentinasc30@users.noreply.github.com>
2023-11-21 14:35:16 -0500

    exportar

--- 06-Trimestre 6/JSJV/inventario/admin.py
+++ 06-Trimestre 6/JSJV/inventario/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Machine,Inventory,Input
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 @admin.register(Input)
 class InputAdmin(admin.ModelAdmin):
