40a3e4dc216a107fa8d85f7a48fb2f4363603740
valentinasc30 <105883219+valentinasc30@users.noreply.github.com>
2023-12-01 14:10:40 -0500

    diseño

--- 06-Trimestre 6/JSJV/core/admin.py
+++ 06-Trimestre 6/JSJV/core/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 from .models import Category,Product
 
 #admin.site.register(Category)
 #admin.site.register(Product)
+admin.site.site_header = 'Bienvenido'
+
+admin.site.index_title = 'JSJV'
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -16,4 +20,5 @@ class ProductAdmin(admin.ModelAdmin):
     search_fields=('name',)
     list_filter=('category',)
     list_per_page=2
+
 
